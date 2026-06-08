fd70e68e87c45bc01b0875a53c710d28073be6bb
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-08 16:19:25 +0530

    Added cloudefare protection

---
+++ functions/[[path]].js
@@ -0,0 +1,16 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  // Allow login page without cookie
+  if (url.pathname === '/login.html') {
+    return context.next();
+  }
+
+  const cookie = context.request.headers.get('Cookie') || '';
+
+  if (!cookie.includes('authToken=')) {
+    return Response.redirect(`${url.origin}/login.html`, 302);
+  }
+
+  return context.next();
+}
