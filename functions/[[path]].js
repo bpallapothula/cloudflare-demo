96fe28e46d1d69c2280cf5e8978e63ec2e91d4b9
Balakrishna <bpallapothula@wearebarbarian.com>
2026-06-08 16:45:00 +0530

    test function

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,16 +1,3 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-
-  // Allow login page
-  if (url.pathname === "/login.html") {
-    return context.next();
-  }
-
-  const cookie = context.request.headers.get("Cookie") || "";
-
-  if (!cookie.includes("authToken=")) {
-    return Response.redirect(`${url.origin}/login.html`, 302);
-  }
-
-  return context.next();
+export async function onRequest() {
+  return new Response("Function is running!");
 }
